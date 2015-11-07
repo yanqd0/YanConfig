6bc4a4c72405626633400641e2c92e01d6852e51
yanqd0 <yanqd0@gmail.com>
2015-11-07 21:02:41 +0800

    Support multiple vimrc files.

--- .shell/install.sh
+++ .shell/install.sh
@@ -2,14 +2,23 @@
 
 CONFIG_DIR="MacOSX"
 
-VIMRC=~/.vim/vimrc
-if [[ -f $VIMRC ]]; then
-    echo $VIMRC exists!
+function install_before_check {
+file=$1
+TARGET_FILE=$2/$file
+
+if [ -f $TARGET_FILE ]; then
+    echo $TARGET_FILE exists!
     echo Try make update instead.
     exit
 else
-    cp $CONFIG_DIR/vim/vimrc $VIMRC
-    echo $VIMRC is installed.
+    cp $CONFIG_DIR/vim/$file $TARGET_FILE
+    echo $TARGET_FILE is installed.
 fi
+}
 
+FILE_LIST=`ls $CONFIG_DIR/vim/`
+for file in $FILE_LIST
+do
+    install_before_check $file ~/.vim
+done
 echo Done.
