dfcc67d54e30ffa93f350c8f6d17a43fb9512f67
yanqd0 <yanqd0@gmail.com>
2015-11-15 22:21:45 +0800

    Initialize bash configs in MacOSX, #17

---
+++ MacOSX/.alias.bash
@@ -0,0 +1,69 @@
+# YanConfig Copyright (C) 2015 yanqd0@gmail.com
+#
+# This program comes with ABSOLUTELY NO WARRANTY under the terms of GPLv3.
+# This is free software, and you are welcome to redistribute it under certain
+# conditions; see the LISENSE in the root of this project.
+
+# ls
+# List files and directories with human readable detailed infomation
+alias ll='ls -hlF'
+# Show hidden files only
+alias l.='ls -d .*'
+# Show files only
+alias lsf='ls -hl | grep ^d'
+# Show directories only
+alias lsd='ls -hl | grep -v ^d'
+# Recursive directory listing
+alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'''
+
+# Jump back n directories at a time
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+
+# git
+# Compact, colorized git log
+alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# Visualise git log (like gitk, in the terminal)
+alias glg='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
+
+# Human readable df
+alias df='df -h'
+# rm will be more safe by reminding you if without -f behind
+alias rm='rm -i'
+# Show which commands you use the most
+alias freq='cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30'
+# Search for process
+alias tm='ps -ef | grep'
+# Set vi as vim
+alias vi='vim'
+
+# function
+# mkdir and cd into
+function mcd() {
+mkdir -p "$1" && cd "$1";
+}
+# extract files
+myextract () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xjf $1     ;;
+            *.tar.gz)    tar xzf $1     ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar e $1     ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xf $1      ;;
+            *.tbz2)      tar xjf $1     ;;
+            *.tgz)       tar xzf $1     ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)     echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
---
+++ MacOSX/.bash_profile
@@ -0,0 +1,32 @@
+# YanConfig Copyright (C) 2015 yanqd0@gmail.com
+#
+# This program comes with ABSOLUTELY NO WARRANTY under the terms of GPLv3.
+# This is free software, and you are welcome to redistribute it under certain
+# conditions; see the LISENSE in the root of this project.
+
+set completion-ignore-case on
+set show-all-if-ambiguous on
+
+# Setting bash-completion
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion;
+fi
+
+# Setting git completion
+source ~/.git-completion.bash
+
+# Persinal bash profile #
+# ls
+export CLICOLOR=1   #ls color
+export CLICOLOR_FORCE=1
+export LSCOLORS=gxfxaxdxcxegedabagacad  #ls color specific
+# grep
+export GREP_OPTIONS='--color=always'
+export GREP_COLOR='5;40'
+
+# alias
+source ~/.alias.bash
+
+# Setting Android SDK path
+PATH="~/Library/Android/sdk/platform-tools:${PATH}"
+export PATH
