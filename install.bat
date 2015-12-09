82d0a08c57d28f815b981608edaad126bbc7bf7d
yanqd0 <yanqd1@lenovo.com>
2015-12-09 17:17:42 +0800

    Enhance the `install.bat`

--- install.bat
+++ install.bat
@@ -1,1 +1,7 @@
-xcopy Windows %USERPROFILE% /i /y
+:: YanConfig Copyright (C) 2015 yanqd0@gmail.com
+::
+:: This program comes with ABSOLUTELY NO WARRANTY under the terms of GPLv3.
+:: This is free software, and you are welcome to redistribute it under certain
+:: conditions; see the LISENSE in the root of this project.
+
+xcopy /s /i /y Windows %USERPROFILE%
